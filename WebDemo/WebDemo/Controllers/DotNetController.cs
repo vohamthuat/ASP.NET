786882012a1dd28155dd95ff19e62929400a480b
Võ Thuật <vohamthuat@gmail.com>
2020-10-17 21:35:13 +0700

    Update

---
+++ WebDemo/WebDemo/Controllers/DotNetController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebDemo.Models;
+
+namespace WebDemo.Controllers
+{
+    public class DotNetController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View("MainView");
+        }
+
+        public ActionResult Act(string button)
+        {
+            return View("MainView");
+        }
+
+        public ActionResult LoadStudent()
+        {
+            Student s1 = new Student()
+            {
+                ID = 1001,
+                FullName = "Alpha",
+                DoB = new DateTime(2000, 1, 1)
+            };
+
+            return View("MainView", s1);
+        }
+    }
+}
---
+++ WebDemo/WebDemo/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebDemo.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string FullName { get; set; }
+        public DateTime DoB { get; set; }
+
+        public int GetAges()
+        {
+            return (DoB.Year - DateTime.Now.Year);
+        }
+    }
+}
